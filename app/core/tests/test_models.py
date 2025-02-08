1dfc258d671699e853f125d1c0a084948790dcb8
salahoddin <salahoddin.sambutuan89@gmail.com>
2025-02-08 16:45:20 +0800

    Implement ingredients api

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -78,3 +78,13 @@ class ModelTests(TestCase):
         tag = models.Tag.objects.create(user=user, name='Sample tag')
 
         self.assertEqual(str(tag), tag.name)
+
+    def test_create_ingredient(self):
+        """Test creating an ingredient is successful."""
+        user = create_user()
+        ingredient = models.Ingredient.objects.create(
+            user=user,
+            name='Sample ingredient'
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -10,7 +10,8 @@ from rest_framework.permissions import IsAuthenticated
 
 from core.models import (
     Recipe,
-    Tag
+    Tag,
+    Ingredient
 )
 from recipe import serializers
 
@@ -55,3 +56,18 @@ class TagViewSet(mixins.DestroyModelMixin,
     def perform_create(self, serializer):
         """Create a new tag."""
         serializer.save(user=self.request.user)
+
+
+class IngredientViewSet(mixins.DestroyModelMixin,
+                        mixins.UpdateModelMixin,
+                        mixins.ListModelMixin,
+                        viewsets.GenericViewSet):
+    """Manage ingredients in the database."""
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Retrieve ingredients for authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
